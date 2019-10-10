1bd0dc255d8ca355fa1e783870be207e516a66b9
Aleksey Korolev <aking@live.ru>
2019-10-10 19:24:08 +0300

    initial

---
+++ 00/sum_by_cols.cpp
@@ -0,0 +1,27 @@
+#include <cstdlib>
+#include <iostream>
+
+#include "timer.h"
+
+#define N 10000
+
+int main() {
+  int (* a)[N] = new int[N][N];
+
+  srand(1827);
+  for (int i = 0; i < N; ++i) {
+	for (int j = 0; j < N; ++j) {
+	  a[i][j] = rand() % 2;
+	}
+  }
+
+  Timer t;
+  int sum = 0;
+  for (int i = 0; i < N; ++i) {
+	for (int j = 0; j < N; ++j) {
+	  sum += a[j][i];
+	}
+  }
+  return 0;
+}
+
