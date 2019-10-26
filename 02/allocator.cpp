43fc930c34d55687ec234ceafed0560bf15ac199
Aleksey Korolev <aking@live.ru>
2019-10-26 07:23:29 +0300

    delete expression -> operator delete

--- 02/allocator.cpp
+++ 02/allocator.cpp
@@ -2,12 +2,12 @@
 
 ak::LinearAllocator::~LinearAllocator()
 {
-    delete m_buffer;
+    operator delete(m_buffer);
 }
 
 ak::LinearAllocator::LinearAllocator(std::size_t maxSize)
 {
-    m_buffer = (char *)(operator new(maxSize));
+    m_buffer = static_cast<char *>(operator new(maxSize));
     m_offset = 0;
     m_size = maxSize;
 }
@@ -15,7 +15,7 @@ ak::LinearAllocator::LinearAllocator(std::size_t maxSize)
 char *ak::LinearAllocator::alloc(size_t req_size)
 {
     if ((m_size - m_offset) < req_size)
-        return NULL;
+        return nullptr;
     char *ret = m_buffer + m_offset;
     m_offset += req_size;
     return ret;
